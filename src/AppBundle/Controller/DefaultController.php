c3451202a53553d9325c83c2c8d61dcacf8f3a2a
Urazov Dmitriy (home) <urazov.d@gmail.com>
2015-10-06 00:25:59 +0300

    create example

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,10 +9,24 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/test", name="homepage")
      */
     public function indexAction(Request $request)
     {
+        $host="Vpn.shket-it.ru:1433";
+        $user="sa";
+        $pwd="VFLFUFCRFH";
+        $db_name = "DB_SHKET";
+
+        $conn = mssql_connect ($host,$user,$pwd)
+        or die ("Can't connect to Microsoft SQL Server");
+
+        mssql_select_db($db_name, $conn) or die ("Can't select databes");
+
+
+        var_dump($conn);
+        exit;
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
