83083327498fc2bcba1866be94910a80db1741c6
Urazov Dmitriy <urazov.d@gmail.com>
2015-10-07 09:50:52 +0300

    create base template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,23 +9,27 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/test", name="homepage")
+     * @Route("/testdb", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function testAction(Request $request)
     {
-	$host="192.168.88.254";
+	    $host="192.168.88.254";
         $user="sa";
         $pwd="VFLFUFCRFH";
         $db_name = "DB_TEST";
 
-	$db = new \PDO("dblib:host=192.168.88.254;dbname=DB_TEST", $user, $pwd);
+	    $db = new \PDO("dblib:host=$host;dbname=$db_name", $user, $pwd);
 
-	var_dump($db);exit;
-        exit;
+        if(!$db){
+            $status = 'fail';
+        } else {
+            $status = 'ok';
+        }
 
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'status' => $status
         ));
     }
 }
