b0a5b0c1e224719375ddae2a7682bd436fcc0cbc
Urazov Dmitriy <urazov.d@gmail.com>
2015-10-21 00:14:14 +0300

    add redirect, printing, sending post

--- src/MainBundle/Controller/MainController.php
+++ src/MainBundle/Controller/MainController.php
@@ -2,8 +2,11 @@
 
 namespace MainBundle\Controller;
 
+use Exception;
 use MainBundle\Utils\DB;
+use Swift_Message;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends Controller
@@ -24,5 +27,31 @@ class MainController extends Controller
     {
         $count = DB::getInstance()->getCountSchool()['cnt'];
         return new Response($count);
+    }
+
+    public function postAction(Request $request)
+    {
+        try{
+
+            $parameters['fio'] = $request->get('fio');
+            $parameters['phone'] = $request->get('phone');
+            $parameters['email'] = $request->get('email');
+            $parameters['msg'] = $request->get('msg');
+
+            $message = Swift_Message::newInstance()
+                ->setSubject('Письмо с сайта')
+                ->setFrom('shket.it.mailer@gmail.com', 'ШКЭТ')
+                ->setTo($parameters['email'])
+                ->setBody($this->renderView(
+                    'MainBundle:Main:mail.html.twig',
+                    array('params' => $parameters)
+                ),'text/html');
+
+            $this->get('mailer')->send($message);
+
+            return new Response('Отправлено');
+        } catch (Exception $e) {
+            return new Response($e->getMessage());
+        }
     }
 }
