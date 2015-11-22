0eb3d1b34abc8a2467f5a80840bacea2178efb89
Urazov Dmitriy <urazov.d@gmail.com>
2015-11-22 22:24:54 +0300

    create index page for boss

--- src/CabinetBundle/Controller/BossController.php
+++ src/CabinetBundle/Controller/BossController.php
@@ -3,11 +3,68 @@
 namespace CabinetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use CabinetBundle\Repositories\Boss\DBBoss;
 
 class BossController extends Controller
 {
     public function indexAction()
     {
-        var_dump('boss');exit;
+        $context = [
+            'time' => date('Y-m-d H:i:s'),
+            'function' => __METHOD__
+        ];
+
+        try{
+            $user = $this->getUser();
+            if(!$user) throw new AuthenticationException('User was not founded');
+            $context['user_id'] = $this->getUser()->getId();
+
+            $result = DBBoss::getInstance()->getUserInfo($user);
+
+            $parameters = [
+                'current_date' => date("d-m-Y"),
+                'last_date' => date("d-m-Y", time() - 7 * 24 * 60 * 60),
+                'mail_name' => $user->getFullName(),
+                'mail_phone' => $user->getPhone(),
+                'mail_email' => $user->getEmail(),
+            ];
+
+            return $this->render('CabinetBundle:Boss:index.html.twig', $parameters);
+        } catch (Exception $e) {
+            $this->get('logger')->error($e->getMessage(), $context);
+            return new Response('Ошибка. Обратитесь к администратору');
+        }
+    }
+
+    public function userInformationAction()
+    {
+        $context = [
+            'time' => date('Y-m-d H:i:s'),
+            'function' => __METHOD__
+        ];
+
+        try{
+            $user = $this->getUser();
+            if(!$user) throw new AuthenticationException('User was not founded');
+            $context['user_id'] = $this->getUser()->getId();
+
+            $result = DBBoss::getInstance()->getUserInfo($user);
+
+            $template_parameters['info'] = $result;
+
+            $template_parameters['full_name'] = $user->getFullName();
+            $template_parameters['phone'] = $user->getPhone();
+            $template_parameters['email'] = $user->getEmail();
+            $template_parameters['usr_id'] = $user->getId();
+            $ava_path = realpath($this->container->getParameter('kernel.root_dir').'/../web/users/').'/'.$user->getId().'/avatar.jpg';
+            if(file_exists($ava_path)){
+                $template_parameters['avatar'] = $ava_path;
+            }
+
+            return $this->render('CabinetBundle:Teacher:user_information.html.twig', $template_parameters);
+        } catch (Exception $e) {
+            $this->get('logger')->error($e->getMessage(), $context);
+            return new Response('Ошибка. Обратитесь к администратору');
+        }
     }
 }
---
+++ src/CabinetBundle/Repositories/Boss/DBBoss.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace CabinetBundle\Repositories\Boss;
+
+use MainBundle\Security\User;
+use MainBundle\Utils\DB;
+use PDO;
+
+class DBBoss
+{
+    /**
+     * @var DB
+     */
+    private static $db_instance;
+
+    private static $instance = null;
+
+    private function __construct()
+    {
+        self::$db_instance = DB::getInstance();
+    }
+
+    /**
+     * @return DBBoss
+     */
+    public static function getInstance()
+    {
+        if(is_null(self::$instance)){
+            self::$instance = new DBBoss();
+        }
+        return self::$instance;
+    }
+
+    public static function getUserInfo(User $user)
+    {
+        $query = "SELECT DISTINCT s.SCL_ID, s.NAME, c.cls_id, c.name
+                  FROM CS_SHKET.USR u
+                       INNER JOIN CS_SHKET.USER_IN_SCL_CLS uc on u.USR_ID = uc.USR_ID
+                       INNER JOIN CS_SHKET.CLS c on c.SCL_ID = uc.SCL_ID AND c.CLS_ID = uc.cls_id
+                       INNER JOIN CS_SHKET.scl s on s.SCL_ID = uc.SCL_ID
+                  WHERE u.ROLE_ID = ? and u.del <> 1 and uc.del<> 1 and c.del <> 1 and s.del <> 1
+                  AND u.USR_ID = ?";
+
+        $result = DB::getInstance()->getAll($query, [$user->getRoleId(), $user->getId()]);
+
+        return $result;
+    }
+}
