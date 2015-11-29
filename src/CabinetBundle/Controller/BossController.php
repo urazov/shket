9b0cb7c87c2a3b2cedaa445c460cb142d5e88c8b
Urazov Dmitriy <urazov.d@gmail.com>
2015-11-29 13:01:37 +0300

    add reports for boss

--- src/CabinetBundle/Controller/BossController.php
+++ src/CabinetBundle/Controller/BossController.php
@@ -4,6 +4,9 @@ namespace CabinetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use CabinetBundle\Repositories\Boss\DBBoss;
+use CabinetBundle\Repositories\Teacher\DBTeacher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class BossController extends Controller
 {
@@ -21,13 +24,18 @@ class BossController extends Controller
 
             $result = DBBoss::getInstance()->getUserInfo($user);
 
+            $all_classes = DBBoss::getInstance()->getAllClasses($user);
+
             $parameters = [
+                'current_year' => date("Y", time()),
+                'current_month' => date("m", time()),
                 'current_date' => date("d-m-Y"),
                 'last_date' => date("d-m-Y", time() - 7 * 24 * 60 * 60),
                 'mail_name' => $user->getFullName(),
                 'mail_phone' => $user->getPhone(),
                 'mail_email' => $user->getEmail(),
                 'school' => $result,
+                'classes' => $all_classes
             ];
 
             return $this->render('CabinetBundle:Boss:index.html.twig', $parameters);
@@ -66,6 +74,86 @@ class BossController extends Controller
         } catch (Exception $e) {
             $this->get('logger')->error($e->getMessage(), $context);
             return new Response('Ошибка. Обратитесь к администратору');
+        }
+    }
+
+    public function tabelAction(Request $request)
+    {
+        $context = [
+            'time' => date('Y-m-d H:i:s'),
+            'function' => __METHOD__
+        ];
+
+        try{
+            $user = $this->getUser();
+            if(!$user) throw new AuthenticationException('User was not founded');
+            $context['user_id'] = $this->getUser()->getId();
+
+            $info = DBBoss::getInstance()->getUserInfo($user);
+
+            $parameters = [
+                'month' => $request->get('month'),
+                'year' => $request->get('year'),
+                'school_id' => $info[0]['SCL_ID'],
+                'class_id' => $request->get('class_id')
+            ];
+
+            $days_in_month = cal_days_in_month(CAL_GREGORIAN, $parameters['month'], $parameters['year']);
+
+            $pupils = DBBoss::getInstance()->getAllPupil($parameters);
+
+            $parameters['date_to'] = $parameters['year'] . '-' . ($parameters['month']+1) . '-01';
+            $parameters['date_from'] = $parameters['year'] . '-' . $parameters['month'] . '-01';
+
+            foreach($pupils as $pupil){
+                $user_total = 0;
+                $food_user_date = [];
+                $parameters['pupil_id'] = $pupil['usr_id'];
+                $food_date_info = DBBoss::getInstance()->getFoodCountForBjtPupil($parameters);
+                foreach($food_date_info as $key => $date){
+                    $food_user_date[$date['date']] = $date['cnt'];
+                }
+
+                for($day = 1; $day <= $days_in_month; $day++){
+                    $date = $parameters['year'] . '-' . $parameters['month'] . '-' . ($day<10 ? '0'.$day : $day);
+                    if(array_key_exists($date, $food_user_date)){
+                        $result[$pupil['usr_id']][$day] = $food_user_date[$date];
+                        $user_total += $food_user_date[$date];
+                    } else {
+                        $result[$pupil['usr_id']][$day] = 0;
+                    }
+                }
+                $result[$pupil['usr_id']][$day] = $user_total;
+            }
+
+            $itog_total = 0;
+            $parameters['date_to'] = $parameters['year'] . '-' . ($parameters['month']+1) . '-01';
+            $parameters['date_from'] = $parameters['year'] . '-' . $parameters['month'] . '-01';
+            $itog_result = DBBoss::getInstance()->getFoodBjtCountItog($parameters);
+            $itog_result_date = [];
+            foreach($itog_result as $key => $date){
+                $itog_result_date[$date['date']] = $date['cnt'];
+            }
+            for($day = 1; $day <= $days_in_month; $day++){
+                $date = $parameters['year'] . '-' . $parameters['month'] . '-' . ($day<10 ? '0'.$day : $day);
+                if(array_key_exists($date, $itog_result_date)){
+                    $itog[$day] = $itog_result_date[$date];
+                    $itog_total += $itog[$day];
+                } else {
+                    $itog[$day] = 0;
+                }
+            }
+            $itog[$day] = $itog_total;
+
+            return $this->render('CabinetBundle:Boss/rep_tabel_cat:rep_tabel_cat_report.html.twig', [
+                'result' => isset($result) ? $result : [],
+                'pupils' => $pupils,
+                'days' => $days_in_month,
+                'total' => isset($itog) ? $itog : []
+            ]);
+
+        } catch (Exception $e){
+
         }
     }
 }
--- src/CabinetBundle/Repositories/Boss/DBBoss.php
+++ src/CabinetBundle/Repositories/Boss/DBBoss.php
@@ -45,4 +45,121 @@ class DBBoss
 
         return $result;
     }
+
+    public function getAllClasses(User $user)
+    {
+        $query = "select distinct cls.cls_id, cls.name
+        , CAST(SUBSTRING(cls.name, 1, case when PATINDEX('%[^0-9]%',cls.name) = 0 then LEN(cls.name) else PATINDEX('%[^0-9]%',cls.name)-1 end) as int) as num_cls
+        from cs_shket.user_in_scl_cls as uic, cs_shket.cls as cls
+        where uic.scl_id in (
+                select uic2.scl_id
+                from cs_shket.user_in_scl_cls as uic2
+                where uic2.usr_id = ?
+                and uic2.del <> 1
+        )
+        and cls.cls_id = uic.cls_id
+        and uic.del <> 1 and uic.cls_id <> 0 and cls.del <> 1
+        order by num_cls asc";
+
+        $result = DB::getInstance()->getAll($query, [$user->getId()]);
+
+        return $result;
+    }
+
+    public function getAllPupil($parameters)
+    {
+        $query = 'select u.name, u.usr_id, cls.name as cls_name
+            from cs_shket.usr u, cs_shket.USER_IN_SCL_CLS uc, cs_shket.cls cls
+                where u.del <> 1 and uc.del <> 1
+                    and u.usr_id = uc.usr_id
+                    and uc.cls_id = cls.cls_id
+                    and (uc.CLS_ID = ? or ? = -1)
+                    and uc.scl_id = ?
+                    and role_id = 1
+                    order by u.name asc';
+
+        $result = DB::getInstance()->getAll($query, [$parameters['class_id'], $parameters['class_id'], $parameters['school_id']]);
+        return $result;
+    }
+
+    public function getFoodCountItog($parameters)
+    {
+        $query = "
+            select SUM(cnt) as cnt, ADATE as date
+            from cs_shket.RLS, cs_shket.USER_IN_SCL_CLS uc
+            where (uc.CLS_ID = ? or ? = -1)
+              and rls.usr_id = uc.usr_id and rls.del <> 1 and uc.del <> 1
+              and ADATE >= ?
+              and ADATE < ?
+              and is_cplx = 1
+              and is_bjt = 1
+              group by ADATE
+        ";
+
+        $result = DB::getInstance()->getAll($query, [
+            $parameters['class_id'], $parameters['class_id'], $parameters['date_from'], $parameters['date_to']
+        ]);
+
+        return $result;
+    }
+
+    public function getFoodCountForPupil($parameters)
+    {
+        $query = 'select SUM(CNT) as cnt, ADATE as date
+                    from CS_SHKET.RLS
+                    where USR_ID = ?
+                      and del <> 1
+                      and ADATE >= ?
+                      and ADATE < ?
+                      and is_cplx = 1
+                      group by ADATE';
+
+        $result = DB::getInstance()->getAll($query, [
+            $parameters['pupil_id'], $parameters['date_from'], $parameters['date_to']
+        ]);
+
+        return $result;
+    }
+
+    public function getFoodCountForBjtPupil($parameters)
+    {
+        $query = 'select SUM(rls.CNT) as cnt, rls.ADATE as date
+                    from CS_SHKET.RLS rls, cs_shket.meal meal
+                    where rls.USR_ID = ?
+                      and meal.meal_id = rls.meal_id
+                      and meal.is_complex = 1
+                      and rls.ADATE >= ?
+                      and rls.ADATE < ?
+                      and rls.is_cplx = 1 and rls.del <> 1 and rls.is_bjt = 1 and meal.del <> 1
+                      group by ADATE';
+
+        $result = DB::getInstance()->getAll($query, [
+            $parameters['pupil_id'], $parameters['date_from'], $parameters['date_to']
+        ]);
+
+        return $result;
+    }
+
+    public function getFoodBjtCountItog($parameters)
+    {
+        $query = "
+            select SUM(cnt) as cnt, ADATE as date
+            from cs_shket.rls, cs_shket.USER_IN_SCL_CLS uc, cs_shket.meal meal
+            where (uc.CLS_ID = ? or ? = -1)
+              and rls.usr_id = uc.usr_id and rls.del <> 1 and uc.del <> 1 and meal.del <> 1
+              and ADATE >= ?
+              and ADATE < ?
+              and is_cplx = 1
+              and is_bjt = 1
+              and meal.meal_id = rls.meal_id
+              and meal.is_complex = 1
+              group by ADATE
+        ";
+
+        $result = DB::getInstance()->getAll($query, [
+            $parameters['class_id'], $parameters['class_id'], $parameters['date_from'], $parameters['date_to']
+        ]);
+
+        return $result;
+    }
 }
